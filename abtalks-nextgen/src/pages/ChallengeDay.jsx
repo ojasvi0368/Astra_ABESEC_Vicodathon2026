28512084a18a576e750727b2af539be679c51037
Ojasvi Yadav <ojasviyadav2308@gmail.com>
2026-08-09 19:52:15 +0530

    done

---
+++ abtalks-nextgen/src/pages/ChallengeDay.jsx
@@ -0,0 +1,7 @@
+export default function ChallengeDay() {
+  return (
+    <div>
+      <h1>Day 12 Challenge</h1>
+    </div>
+  );
+}
---
+++ abtalks-nextgen/src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome to your 60 Day Challenge!</p>
+    </div>
+  );
+}
